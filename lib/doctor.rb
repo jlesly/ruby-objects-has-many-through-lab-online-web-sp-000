62694ad6364c94e0cbfb02b0936f8a63c567200b
Lesly Juarez <leslyj2@gmail.com>
2020-12-02 17:46:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,12 @@
 class Doctor
-  attr_accessor :name
+  attr_accessor :name, :appointments
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+    @appointments = []
   end
 
   def self.all
